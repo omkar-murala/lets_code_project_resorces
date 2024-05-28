4810e47418371eefca7684b1697dbdd4ef332839
omkar-murala <omkar.developer1579@gmail.com>
2024-05-28 13:39:09 +0530

    updated the card.tsx

--- src/app/programming/components/Card.tsx
+++ src/app/programming/components/Card.tsx
@@ -1,31 +1,3 @@
-// import Image from 'next/image';
-// import Link from 'next/link';
-
-// export function Card({ name, image, link }: { image: string, name: string, link: string }) {
-//     return (
-//         <div className="w-[300px] rounded-md border">
-//             <Image
-//                 src={image}
-//                 alt="img"
-//                 className="h-[250px] w-full rounded-md object-contain"
-//             />
-//             <div className="p-4">
-//                 <h1 className="text-lg font-semibold">{name}</h1>
-//                 <p className="mt-3 text-sm text-gray-600">
-//                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-//                 </p>
-//                 <Link href={link}>
-//                     <button
-//                         type="button"
-//                         className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-//                     >
-//                         See More
-//                     </button>
-//                 </Link>
-//             </div>
-//         </div>
-//     )
-// }
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -58,4 +30,3 @@ export function Card({ name, image, link }: { image: string, name: string, link:
         </div>
     );
 }
-
