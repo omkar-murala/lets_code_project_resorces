f264e04231842d8da67bf101e9d921d4ccb88692
omkar-murala <omkar.developer1579@gmail.com>
2024-06-14 21:26:53 +0530

    added new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <meta name="google-adsense-account" content="ca-pub-8907718595878644"></meta>
       <body className={inter.className}>
-      <Script  id="G-Q0QC4R4RC9" strategy="lazyOnload" src={"https://www.googletagmanager.com/gtag/js?id=G-Q0QC4R4RC9"}></Script>
+      <Script  id="G-Q0QC4R4RC9" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-Q0QC4R4RC9`}/>
           <Script 
           id="google-analytics"       
       >{
@@ -37,25 +37,10 @@ export default function RootLayout({
       `
       }
       </Script>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8907718595878644" crossOrigin="anonymous"></Script>
+      <Script strategy="lazyOnload" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8907718595878644" crossOrigin="anonymous"/>
           <Navbar />
           {children}
           <Footer />
-          {/* <Script  id="G-Q0QC4R4RC9" async src="https://www.googletagmanager.com/gtag/js?id=G-Q0QC4R4RC9"></Script>
-          <Script 
-           id="google-analytics"
-          dangerouslySetInnerHTML={{
-            __html:`
-             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-Q0QC4R4RC9');
-            `
-          }}
-          
-></Script>
-<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8907718595878644" crossOrigin="anonymous"></Script> */}
-
 
       </body>
     </html>
