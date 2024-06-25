b62a37e1bf695f946d101aedd2416a17ebe1612a
Shu12388y <spl.sp999@gmail.com>
2024-06-25 14:27:57 +0530

    updated the domain

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const MONGODATABASE:string = process.env.DATABASE_URL!;
 export const PRODUCTIONDOMAIN:string = process.env.PROD_DOMAIN!; 
-export const DOMAIN:string = "http://ec2-13-201-84-213.ap-south-1.compute.amazonaws.com:443"
+export const DOMAIN:string = "https://resourcebackend-4zdy.onrender.com"
